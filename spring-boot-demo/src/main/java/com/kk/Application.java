5cc2f9274e0be761263e34dec52c649f34087e20
zhihui.kzh <563150913@qq.com>
2017-03-27 10:19:08 +0800

    spring boot

--- spring-boot-demo/src/main/java/com/kk/Application.java
+++ spring-boot-demo/src/main/java/com/kk/Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 /**
  * 启动类
